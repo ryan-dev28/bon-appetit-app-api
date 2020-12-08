7ce37c4c5b5bb326076c37a5470f05d631acb203
Ryan Kenth Morales <ryankenthmorales@gmail.com>
2020-12-08 22:09:35 +0800

    Updated admin to support custome user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,3 +26,20 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_users_change_page(self):
+        """Test that the user edit page works"""
+        # Admin edit page
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """"Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+
